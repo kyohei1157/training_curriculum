1e3c8467413c7810e5ef7c445fa62d327fe6a11e
kyohei1157 <71009388+kyohei1157@users.noreply.github.com>
2020-11-04 18:08:01 +0900

    lssue6

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -35,8 +35,8 @@ class CalendarsController < ApplicationController
       plan = plans.map do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      
-      days = {month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wday: wdaysから値を取り出す記述}
+
+      days = {month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wday: (@todays_date+x)}
       @week_days.push(days)
     end
 
